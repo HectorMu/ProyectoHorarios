b00acfea155e30fa5441e4835f8d8fecdda1f32f
Hector Manuel Muñoz Flores <42363909+HectorMu@users.noreply.github.com>
2021-12-07 15:02:37 -0600

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,17 +1,17 @@
 const keys = {
-    // database: {
-    //     host: process.env.DB_HOST,
-    //     user: process.env.DB_USER,
-    //     password: process.env.DB_PASS,
-    //     database: process.env.DB
-    // }
+     database: {
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASS,
+         database: process.env.DB
+    }
 
-    database: {
+   /* database: {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'horariostec'
-    }
+    }*/
 }
 
 module.exports = keys
